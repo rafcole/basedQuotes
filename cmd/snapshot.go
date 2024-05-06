49cd80071e571dbf21e08b623a935bd11f7fccfb
rafcole <39354464+rafcole@users.noreply.github.com>
2024-05-05 17:47:43 -0700

    successful ping of mongodb and env loading from parent dir

--- cmd/snapshot.go
+++ cmd/snapshot.go
@@ -1,13 +1,19 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/spf13/cobra"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func takeSnapShot(cmd *cobra.Command, args []string) {
@@ -46,4 +52,34 @@ func takeSnapShot(cmd *cobra.Command, args []string) {
 
 	fmt.Printf("\tResponse from %s: \n%s\n", venue, string(responseBody))
 
+	connectToServer()
+
+}
+
+func connectToServer() {
+	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
+	// Seems less than ideal for reducing latency
+	godotenv.Load()
+	var accessStr = os.Getenv("MONGO_CONNECTION_STR")
+
+	fmt.Println("access str => ", accessStr)
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+
+	opts := options.Client().ApplyURI(accessStr).SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	// Send a ping to confirm a successful connection
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
 }
