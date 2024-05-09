2f5988247b9c06487c000e9d6970bfe551873ead
rafcole <39354464+rafcole@users.noreply.github.com>
2024-05-09 10:59:47 -0700

    improved error handling for FormatOHLCV

--- adapters/sfox/sfox.go
+++ adapters/sfox/sfox.go
@@ -3,6 +3,7 @@ package sfox
 import (
 	"cryptoSnapShot/adapters"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -18,7 +19,7 @@ type SFOX struct {
 
 func (sfox SFOX) Authenticate() (int, error) {
 	// sFox API is public, no need to authenticate
-	fmt.Println("===> Status: Authenticating")
+	fmt.Println("\n===> Status: Authenticating")
 	return 1, nil
 }
 
@@ -33,13 +34,13 @@ func (sfox SFOX) ValidatePair() (bool, error) {
 		fmt.Println("Error creating request:", err)
 	}
 	godotenv.Load()
-	fmt.Println("API key -> ", os.Getenv("SFOX_API_KEY"))
+	// fmt.Println("API key -> ", os.Getenv("SFOX_API_KEY"))
 
 	// Add custom headers to the request
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("SFOX_API_KEY")))
 
-	fmt.Println(req.Header)
+	// fmt.Println(req.Header)
 
 	// Make the HTTP request
 	client := http.DefaultClient
@@ -81,15 +82,29 @@ func (sfox SFOX) FormattedCurrencyPair() string {
 func (sfox SFOX) FetchOHLCV(q adapters.Query) adapters.OHLCVData {
 	sfox.Authenticate()
 
-	var url = fmt.Sprintf("https://chartdata.sfox.com/candlesticks?endTime=%d&pair=%s&period=60&startTime=%d", sfox.Query.Time_stamp, sfox.FormattedCurrencyPair(), q.StartTime())
+	var url = fmt.Sprintf("https://chartdata.sfox.com/candlesticks?endTime=%d&pair=%s&period=60&startTime=%d", sfox.Query.Time_stamp, sfox.FormattedCurrencyPair(), sfox.Query.StartTime())
+
+	fmt.Println(url)
 
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatalln(err)
 	}
+
 	defer resp.Body.Close()
 
-	ohlcv := sfox.FormatOHLCV(resp.Body)
+	fmt.Println("\n===> API Response status: ", resp.Status)
+	// body, err := io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println("Body to string: ", string(body))
+
+	ohlcv, err := sfox.FormatOHLCV(resp.Body)
+	if err != nil {
+		log.Fatalln("Could not format OHLCV: ", err)
+	}
 
 	fmt.Println(ohlcv)
 
@@ -97,7 +112,7 @@ func (sfox SFOX) FetchOHLCV(q adapters.Query) adapters.OHLCVData {
 }
 
 // takes slice of candlesticks, returns array of OHLCV structs
-func (sfox SFOX) FormatOHLCV(response io.ReadCloser) adapters.OHLCVData {
+func (sfox SFOX) FormatOHLCV(response io.ReadCloser) (adapters.OHLCVData, error) {
 	fmt.Println("===> Response recieved, parsing data")
 	var dataArr []adapters.OHLCVData
 
@@ -107,7 +122,11 @@ func (sfox SFOX) FormatOHLCV(response io.ReadCloser) adapters.OHLCVData {
 		log.Fatalln(err)
 	}
 
-	fmt.Println(" ========> FormatOHLCV candlestick: ", dataArr[0])
+	fmt.Println(" ========> FormatOHLCV candlestick: ", dataArr)
+
+	if len(dataArr) == 0 {
+		return adapters.OHLCVData{}, errors.New("no data available for this period")
+	}
 
 	// This is a hardcode to extract the only element in the array
 	// In a more sophisticated program it may be necessary to process
@@ -115,5 +134,5 @@ func (sfox SFOX) FormatOHLCV(response io.ReadCloser) adapters.OHLCVData {
 	// docs https://pkg.go.dev/encoding/json@go1.22.2#NewDecoder:~:text=an%20input%20stream.-,Example,-%C2%B6
 	candlestick := dataArr[0]
 
-	return candlestick
+	return candlestick, nil
 }
